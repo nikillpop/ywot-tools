a611f77a04a599c98073982b003b796a410489ec
CuteLifeBot <cutelifebot@gmail.com>
2015-02-19 16:02:29 -0500

    Added West/East road functions

--- src/road.js
+++ src/road.js
@@ -1,4 +1,4 @@
-function drawRoad(startPos) {
+function drawEastRoad(startPos) {
     var pos;
     if(startPos) {
         pos = startPos;
@@ -16,6 +16,38 @@ function drawRoad(startPos) {
                 we.move('down');
             }
             we.move('right');
+            we.goToCartesian(we.getCartesian()[0],pos[1]);
+        }
+        pos = we.getCartesian();
+    }
+    if(we.go) {
+        setTimeout(
+            function() {
+                drawRoad(pos);
+            },
+            10
+        );
+    }
+}
+
+function drawWestRoad(startPos) {
+    var pos;
+    if(startPos) {
+        pos = startPos;
+    } else {
+        pos = we.doGetCartesian();
+        we.goToCursor();
+    }
+    if(we.isReady()) {
+        var road;
+        for(var count = 0; count < 16; count++) {
+            road = "▒██   ██▒";
+            if(we.getCartesian()[0]%3==0) { road = "▒██ ═ ██▒"; }
+            for(var idx = 0; idx < road.length; idx++) {
+                we.typeText(road[idx]);
+                we.move('down');
+            }
+            we.move('left');
             we.goToCartesian(we.getCartesian()[0],pos[1]);
         }
         pos = we.getCartesian();
