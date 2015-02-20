db8e33f479af72f5135dc5125ab7775c7aa03f72
CuteLifeBot <cutelifebot@gmail.com>
2015-02-20 13:29:40 -0500

    Fixed path in copyjs

--- copyjs.py
+++ copyjs.py
@@ -2,7 +2,7 @@ import os
 from pyperclip import copy
 
 code = ""
-for filename in os.listdir('.'):
+for filename in os.listdir('src'):
     if os.path.splitext(filename)[-1] == '.js':
         with open(filename, 'r') as f:
             code += f.read() + "\n"
